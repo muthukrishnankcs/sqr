ce462c1908f73d75d469e089fa17c7606c1659c4
Shankar A K <shankar.krishnarao@imaginea.com>
2018-12-31 15:50:05 +0530

    Covered 10 repo create edit delete sceanrio~

--- src/test/java/files/TestCases/Repository_Scenarios_Using_FactoryMethod.java
+++ src/test/java/files/TestCases/Repository_Scenarios_Using_FactoryMethod.java
@@ -22,25 +22,52 @@ public class Repository_Scenarios_Using_FactoryMethod extends BaseClass {
 
 
     @Test
-    public void CreateRepositoryAndDeleteRepositoryUsingFactoryMethod()  {
+    public void CreateRepositoryAndDeleteRepositoryUsingFactoryMethod() {
 
-        System.out.println("Name is        :"+NAME);
-        System.out.println("Description is :"+DESCRIPTION);
+        System.out.println("Name is        :" + NAME);
+        System.out.println("Description is :" + DESCRIPTION);
         // Below Code will create Repo with DataProvider
         CreateAndModifyRepository payload = new CreateAndModifyRepository();
         payload.setName(String.valueOf(NAME));
         payload.setDescription(DESCRIPTION);
+        System.out.println("Repo Need to be Created:" + payload.getName());
+        OutcomeRepositoryResponse createResponse = GitRepoMethods.createRepo(payload);
+        Assert.assertEquals("Expected Name and Actual Name is Not Same", payload.getName(), createResponse.getName());
+
+        // Below Code will Delete Repo which created in first step
+        System.out.println("Repo Need to be Deleted:" + payload.getName());
+
+        Response deleteResponse = GitRepoMethods.deleteRepo(createResponse);
+    }
+    @Test
+    public void CreateRepositoryAndEditRepositoryAndDeleteRepository()  {
+        // Below Code will Create New Repository
+        System.out.println("Name is        :" + NAME);
+        System.out.println("Description is :" + DESCRIPTION);
+        CreateAndModifyRepository payload = new CreateAndModifyRepository();
+        payload.setName(String.valueOf(NAME));
+        payload.setDescription(DESCRIPTION);
+//        payload.setName(String.valueOf(value));
         System.out.println("Repo Need to be Created:"+payload.getName());
         OutcomeRepositoryResponse createResponse = GitRepoMethods.createRepo(payload);
         Assert.assertEquals("Expected Name and Actual Name is Not Same",payload.getName(),createResponse.getName());
 
+        // Below Code will modify Repo which created in first step
+        CreateAndModifyRepository modifyPayload = new CreateAndModifyRepository();
+        modifyPayload.setName(NAME+"Modified");
+        System.out.println("Repo Need to be Modified with this name:"+modifyPayload.getName());
+        OutcomeRepositoryResponse modifyResponse  = GitRepoMethods.modifyRepo(modifyPayload,payload);
+        System.out.println("Modified Response Name is :"+modifyResponse.getName());
+        Assert.assertEquals("Verified Modified Name is not Present",modifyPayload.getName(),modifyResponse.getName());
+
         // Below Code will Delete Repo which created in first step
-        System.out.println("Repo Need to be Deleted:"+payload.getName());
+        System.out.println("Repo Need to be Deleted:" + modifyResponse.getName());
 
-        Response deleteResponse = GitRepoMethods.deleteRepo(createResponse);
-
-
+        Response deleteResponse = GitRepoMethods.deleteRepo(modifyResponse);
 
     }
+
+
+
 
 }
